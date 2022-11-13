694c275884223b9112d25dffebc3cdd34dcee242
juandolce <juandolce20@gmail.com>
2022-11-13 12:18:25 -0300

    first commit

---
+++ components/index.js
@@ -0,0 +1,22 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+import { SubInfo, People, EndDate, EthPrice, NFTTitle } from "./SubInfo";
+
+export {
+    HomeHeader,
+    FocusedStatusBar,
+    NFTCard,
+    CircleButton,
+    RectButton,
+    DetailsDesc,
+    DetailsBid,
+    SubInfo,
+    People,
+    EndDate,
+    EthPrice,
+    NFTTitle
+}
